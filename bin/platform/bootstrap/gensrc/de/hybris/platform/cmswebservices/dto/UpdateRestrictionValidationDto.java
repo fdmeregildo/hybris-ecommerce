3a4ca30ac332c4f8a5fa18738102893b053d28c7
Felipe Meregildo <fdmeregildo@gmail.com>
2017-12-28 17:29:48 -0500

    Initial commit
    first commit

---
+++ bin/platform/bootstrap/gensrc/de/hybris/platform/cmswebservices/dto/UpdateRestrictionValidationDto.java
@@ -0,0 +1,34 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN!
+ * --- Generated at 28/12/2017 02:25:05 PM
+ * ----------------------------------------------------------------
+ *
+ * [y] hybris Platform
+ * 
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of SAP 
+ * Hybris ("Confidential Information"). You shall not disclose such 
+ * Confidential Information and shall use it only in accordance with the 
+ * terms of the license agreement you entered into with SAP Hybris.
+ */
+package de.hybris.platform.cmswebservices.dto;
+
+/**
+ * Deprecated since 6.4, no longer used
+ */
+public  class UpdateRestrictionValidationDto extends de.hybris.platform.cmsfacades.dto.UpdateRestrictionValidationDto 
+{
+
+ 
+	
+	public UpdateRestrictionValidationDto()
+	{
+		// default constructor
+	}
+	
+
+
+}
